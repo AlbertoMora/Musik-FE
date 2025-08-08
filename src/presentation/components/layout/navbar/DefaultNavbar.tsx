f5d0f4ed83798d384df6c3a0a2b09ac675cac56b
AlbertoMora <albertoamp95@gmail.com>
2025-08-07 21:38:37 -0600

    fear: navbar menus look & account menu proto-desing

--- src/presentation/components/layout/navbar/DefaultNavbar.tsx
+++ src/presentation/components/layout/navbar/DefaultNavbar.tsx
@@ -1,11 +1,20 @@
 'use client';
 import React, { useState } from 'react';
 
-import { Anchor, Image, Menu, MenuTarget, MenuDropdown, MenuItem, Button } from '@mantine/core';
+import {
+    Anchor,
+    Image,
+    Menu,
+    MenuTarget,
+    MenuDropdown,
+    MenuItem,
+    Button,
+    Avatar,
+} from '@mantine/core';
 
 import '../../../styles/components/navbar.sass';
 import LoginModal from './LoginModal';
-import { IconLogin2 } from '@tabler/icons-react';
+import { IconLogin2, IconUser } from '@tabler/icons-react';
 import SignUpModal from './SignUpModal';
 import DfaModal from './DfaModal';
 import { I18nTypes } from '@/i18n/dictionaries';
@@ -19,44 +28,8 @@ interface INavigationConfigItems {
     title: string;
     url?: string;
     children?: INavigationConfigItems[];
-    requiredPermission?: string;
-    requiredRole?: string;
+    authorizationRelation?: string;
 }
-
-const navigationConfig = (i18n: I18nTypes['app']['navbar']): INavigationConfigItems[] => [
-    {
-        title: i18n.lyrics.title,
-        children: [
-            {
-                title: i18n.lyrics.children.createLyrics,
-                url: '/lyrics/create',
-            },
-            {
-                title: i18n.lyrics.children.stats,
-                url: '/lyrics/statistics',
-            },
-        ],
-    },
-];
-
-const navbarLinks = (i18n: I18nTypes['app']['navbar']) =>
-    navigationConfig(i18n).map((n, i) => (
-        <Menu key={`${i}-${n.title}`}>
-            <MenuTarget>
-                <div className='navbar-item'>{n.title}</div>
-            </MenuTarget>
-
-            <MenuDropdown>
-                {n.children?.map((r, ix) => {
-                    return r.url ? (
-                        <Anchor key={`${ix}${r.title}`} href={r.url}>
-                            <MenuItem>{r.title}</MenuItem>
-                        </Anchor>
-                    ) : null;
-                })}
-            </MenuDropdown>
-        </Menu>
-    ));
 
 const DefaultNavbar = ({ isLogged, i18n }: INavbarProps) => {
     const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
@@ -75,9 +48,12 @@ const DefaultNavbar = ({ isLogged, i18n }: INavbarProps) => {
                         alt='musik logo'
                     />
                 </Anchor>
-                <div className='j-right'>
+                <div className='j-right flex flex-row items-center gap-4'>
                     {isLogged ? (
-                        <>{navbarLinks(i18n)}</>
+                        <>
+                            {navbarLinks(i18n)}
+                            <AccountMenu i18n={i18n.account} />
+                        </>
                     ) : (
                         <Button
                             size='sm'
@@ -118,5 +94,70 @@ const DefaultNavbar = ({ isLogged, i18n }: INavbarProps) => {
         </nav>
     );
 };
+
+interface IAccountMenuProps {
+    i18n: INavbarProps['i18n']['account'];
+}
+
+const AccountMenu = ({ i18n }: IAccountMenuProps) => {
+    return (
+        <Menu>
+            <MenuTarget>
+                <Avatar color='grape' className='cursor-pointer'>
+                    A
+                </Avatar>
+            </MenuTarget>
+
+            <MenuDropdown className='navbar-item-dropdown'>
+                <a className='navbar-item-child' href='/account'>
+                    <MenuItem className='account-menu-item'>
+                        <span className='flex flex-row items-start justify-center gap-2'>
+                            <IconUser size={16} />
+                            {i18n.children.myAccount}
+                        </span>
+                    </MenuItem>
+                </a>
+                <div className='account-menu-signout'>
+                    <Button type='button'>{i18n.children.signout.button}</Button>
+                </div>
+            </MenuDropdown>
+        </Menu>
+    );
+};
+
+const navigationConfig = (i18n: INavbarProps['i18n']): INavigationConfigItems[] => [
+    {
+        title: i18n.lyrics.title,
+        children: [
+            {
+                title: i18n.lyrics.children.createLyrics,
+                url: '/lyrics/create',
+            },
+            {
+                title: i18n.lyrics.children.stats,
+                url: '/lyrics/statistics',
+            },
+        ],
+    },
+];
+
+const navbarLinks = (i18n: INavbarProps['i18n']) =>
+    navigationConfig(i18n).map((n, i) => (
+        <Menu key={`${i}-${n.title}`}>
+            <MenuTarget>
+                <div className='navbar-item'>{n.title}</div>
+            </MenuTarget>
+
+            <MenuDropdown className='navbar-item-dropdown'>
+                {n.children?.map((r, ix) => {
+                    return r.url ? (
+                        <a className='navbar-item-child' key={`${ix}${r.title}`} href={r.url}>
+                            <MenuItem>{r.title}</MenuItem>
+                        </a>
+                    ) : null;
+                })}
+            </MenuDropdown>
+        </Menu>
+    ));
 
 export default DefaultNavbar;
